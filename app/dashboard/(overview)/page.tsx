baf07115596554612554dcf4d460b4a1f276c229
steven-tan-telus <steven.tan@telus.com>
2025-03-26 17:20:52 -0400

    added authentication and metadata

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -10,6 +10,12 @@ import {
   CardsSkeleton,
 } from "@/app/ui/skeletons";
 
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Home | Acme Dashboard",
+};
+
 export default async function Page() {
   const {
     numberOfInvoices,
